2537df72ed0611bc006197a007d755eec32b6621
Michael Arrington <michael@clevercoding.com>
2019-08-20 23:24:28 -0600

    Added tests and updated iterator

--- Sources/LinkedList/LinkedList.swift
+++ Sources/LinkedList/LinkedList.swift
@@ -1,134 +1,96 @@
 import Foundation
 
 public class Node<Element> {
-    public let item: Element
+	public let item: Element
+	
+	private var _previous: Node?
+	private var _next: Node?
+	
+	public init(_ item: Element) {
+		self.item = item
+	}
+}
 
-    private var _previous: Node?
-    private var _next: Node?
-
-    public var previous: Node? {
-        get {
-            return _previous
-        }
-        set {
-            newValue?._previous = _previous
-            newValue?._next = self
-            _previous?._next = newValue
-            self._previous = newValue
-        }
-    }
-
-    public var next: Node? {
-        get {
-            return _next
-        }
-        set {
-            newValue?._previous = self
-            newValue?._next = _next
-            _next?.previous = newValue
-            self._next = newValue
-        }
-    }
-
-    public init(_ item: Element) {
-        self.item = item
-    }
-
-    public var isFirst: Bool {
-        return previous == nil
-    }
-
-    public var isLast: Bool {
-        return next == nil
-    }
-
-    /// The total number of nodes in the list.
-    ///
-    /// - Complexity: O(n), where `n` is the number of items in the list. Duh.
-    public var count: Int {
-        return numberOfItemsBefore + numberOfItemsAfter + 1
-    }
-
-    public var numberOfItemsBefore: Int {
-        guard let previous = _previous else {
-            return 0
-        }
-        return previous.numberOfItemsBefore + 1
-    }
-
-    public var numberOfItemsAfter: Int {
-        guard let next = _next else {
-            return 0
-        }
-        return next.numberOfItemsAfter + 1
-    }
+extension Node {
+	
+	public var previous: Node? {
+		get {
+			return _previous
+		}
+		set {
+			newValue?._previous = _previous
+			newValue?._next = self
+			_previous?._next = newValue
+			self._previous = newValue
+		}
+	}
+	
+	public var next: Node? {
+		get {
+			return _next
+		}
+		set {
+			newValue?._previous = self
+			newValue?._next = _next
+			_next?._previous = newValue
+			self._next = newValue
+		}
+	}
+	
+	public var isFirst: Bool {
+		return previous == nil
+	}
+	
+	public var isLast: Bool {
+		return next == nil
+	}
+	
+	/// The total number of nodes in the list.
+	///
+	/// - Complexity: O(n), where `n` is the number of items in the list. Duh.
+	public var count: Int {
+		return numberOfItemsBefore + numberOfItemsAfter + 1
+	}
+	
+	public var numberOfItemsBefore: Int {
+		guard let p = _previous else {
+			return 0
+		}
+		return p.numberOfItemsBefore + 1
+	}
+	
+	public var numberOfItemsAfter: Int {
+		guard let n = _next else {
+			return 0
+		}
+		return n.numberOfItemsAfter + 1
+	}
 }
 
 extension Node: Sequence {
-
-    public func makeIterator() -> Node<Element>.Iterator {
-        return Iterator(current: self)
-    }
-
-    public struct Iterator: IteratorProtocol {
-        private var current: Node
-
-        fileprivate init(current: Node) {
-            self.current = current
-        }
-
-        public mutating func next() -> Element? {
-            guard let next = current.next else {
-                return nil
-            }
-            return next.item
-        }
-    }
+	
+	public func makeIterator() -> Node<Element>.Iterator {
+		return Iterator(current: self)
+	}
+	
+	public struct Iterator: IteratorProtocol {
+		private var current: Node?
+		
+		fileprivate init(current: Node) {
+			self.current = current
+		}
+		
+		public mutating func next() -> Element? {
+			defer {
+				self.current = current?.next
+			}
+			return current?.item
+		}
+	}
 }
 
-
-let a = Node("A")
-let b = Node("B")
-let c = Node("C")
-let d = Node("D")
-
-a.previous === nil
-a.next === nil
-b.previous === nil
-b.next === nil
-c.previous === nil
-c.next === nil
-d.previous === nil
-d.next === nil
-
-// MARK: -
-d.previous = c
-a.next = c
-
-a.previous === nil
-a.next === c
-b.previous === nil
-b.next === nil
-c.previous === a
-c.next === nil
-
-// MARK: -
-c.previous = b
-
-a.previous === nil
-a.next === b
-b.previous === a
-b.next === c
-c.previous === b
-c.next === nil
-
-// MARK: -
-c.previous = nil
-
-a.previous === nil
-a.next === b
-b.previous === a
-b.next === nil
-c.previous === nil
-c.next === nil
-
+extension Node: Equatable where Element: Equatable {
+	public static func == (left: Node, right: Node) -> Bool {
+		return left.item == right.item
+	}
+}
--- Tests/LinkedListTests/LinkedListTests.swift
+++ Tests/LinkedListTests/LinkedListTests.swift
@@ -2,14 +2,109 @@ import XCTest
 @testable import LinkedList
 
 final class LinkedListTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(LinkedList().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+	func testExample() {
+		let a = Node("A")
+		let b = Node("B")
+		let c = Node("C")
+		
+		XCTAssertEqual(a.previous, nil)
+		XCTAssertEqual(a.next, nil)
+		XCTAssertEqual(b.previous, nil)
+		XCTAssertEqual(b.next, nil)
+		XCTAssertEqual(c.previous, nil)
+		XCTAssertEqual(c.next, nil)
+		
+		XCTAssertEqual(a.count, 1)
+		
+		// MARK: -
+		a.next = c
+		
+		XCTAssertEqual(a.previous, nil)
+		XCTAssertEqual(a.next, c)
+		XCTAssertEqual(b.previous, nil)
+		XCTAssertEqual(b.next, nil)
+		XCTAssertEqual(c.previous, a)
+		XCTAssertEqual(c.next, nil)
+		
+		XCTAssertEqual(a.numberOfItemsBefore, 0)
+		XCTAssertEqual(a.numberOfItemsAfter, 1)
+		XCTAssertEqual(a.count, 2)
+		XCTAssertEqual(b.numberOfItemsBefore, 0)
+		XCTAssertEqual(b.numberOfItemsAfter, 0)
+		XCTAssertEqual(b.count, 1)
+		XCTAssertEqual(c.numberOfItemsBefore, 1)
+		XCTAssertEqual(c.numberOfItemsAfter, 0)
+		XCTAssertEqual(c.count, 2)
+		
+		// MARK: -
+		c.previous = b
+		
+		XCTAssertEqual(a.previous, nil)
+		XCTAssertEqual(a.next, b)
+		XCTAssertEqual(b.previous, a)
+		XCTAssertEqual(b.next, c)
+		XCTAssertEqual(c.previous, b)
+		XCTAssertEqual(c.next, nil)
+		
+		XCTAssertEqual(a.numberOfItemsBefore, 0)
+		XCTAssertEqual(a.numberOfItemsAfter, 2)
+		XCTAssertEqual(a.count, 3)
+		XCTAssertEqual(b.numberOfItemsBefore, 1)
+		XCTAssertEqual(b.numberOfItemsAfter, 1)
+		XCTAssertEqual(b.count, 3)
+		XCTAssertEqual(c.numberOfItemsBefore, 2)
+		XCTAssertEqual(c.numberOfItemsAfter, 0)
+		XCTAssertEqual(c.count, 3)
+		
+		// MARK: -
+		c.previous = nil
+		
+		XCTAssertEqual(a.previous, nil)
+		XCTAssertEqual(a.next, b)
+		XCTAssertEqual(b.previous, a)
+		XCTAssertEqual(b.next, nil)
+		XCTAssertEqual(c.previous, nil)
+		XCTAssertEqual(c.next, nil)
+		
+		XCTAssertEqual(a.numberOfItemsBefore, 0)
+		XCTAssertEqual(a.numberOfItemsAfter, 1)
+		XCTAssertEqual(a.count, 2)
+		XCTAssertEqual(b.numberOfItemsBefore, 1)
+		XCTAssertEqual(b.numberOfItemsAfter, 0)
+		XCTAssertEqual(b.count, 2)
+		XCTAssertEqual(c.numberOfItemsBefore, 0)
+		XCTAssertEqual(c.numberOfItemsAfter, 0)
+		XCTAssertEqual(c.count, 1)
+	}
+	
+	func testIterator() {
+		let a = Node("A")
+		let b = Node("B")
+		let c = Node("C")
+		
+		var count = 0
+		for _ in a {
+			count += 1
+		}
+		XCTAssertEqual(count, 1)
+		
+		a.next = b
+		count = 0
+		for _ in a {
+			count += 1
+		}
+		XCTAssertEqual(count, 2)
+		
+		b.next = c
+		count = 0
+		for item in a {
+			print(item)
+			count += 1
+		}
+		XCTAssertEqual(count, 3)
+	}
+	
+//	static var allTests = [
+//		("testExample", testExample),
+//	]
 }
